3e2d18bb0fc5c9a77165d0100082d05f7a8a09f9
Eduard <eduard.agapia@gmail.com>
2020-01-24 18:10:28 +0200

    RestApi with Spring

---
+++ src/main/java/com/example/conferencedemo/controllers/SpeakerController.java
@@ -0,0 +1,45 @@
+package com.example.conferencedemo.controllers;
+
+import com.example.conferencedemo.models.Speaker;
+import com.example.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
+public class SpeakerController {
+
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> speakerList() {
+        return speakerRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id) {
+        return speakerRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Speaker create(@RequestBody Speaker speaker) {
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        speakerRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id, Speaker speaker) {
+        Speaker existentSpeaker = speakerRepository.getOne(id);
+        BeanUtils.copyProperties(speaker, existentSpeaker, "speaker_id");
+        return speakerRepository.saveAndFlush(speaker);
+    }
+}
